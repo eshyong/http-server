4d00824ad76dbbfb284857f6a0e83f2585f60655
eshyong <eric.shyong@gmail.com>
2014-03-24 01:28:09 -0700

    Added http structs and some parse methods

--- server.cc
+++ server.cc
@@ -3,14 +3,20 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
 
 #define PORT    5000
 #define BACKLOG 10
+
+enum method {
+    UNDEFINED = 0, GET, POST, PUT, DELETE
+};
 
 struct server {
     struct sockaddr_in servaddr;
@@ -23,14 +29,43 @@ struct server {
 };
 
 struct http_request {
-    int version;
-    int status;
+    uint8_t version;
+    enum method mthd;
+    char method_name[7];
+    char uri[1024];
 };
 
 struct http_response {
     int version;
     int status;
 };
+
+enum method get_method(const char *const string) {
+    if (strcmp(string, "GET") == 0) {
+        return GET;
+    } else if (strcmp(string, "POST") == 0) {
+        return POST;
+    } else if (strcmp(string, "PUT") == 0) {
+        return PUT;
+    } else if (strcmp(string, "DELETE") == 0) {
+        return DELETE;
+    } else {
+        return UNDEFINED;
+    }
+}
+
+uint8_t get_version(const char *const string) {
+    if (strcmp(string, "HTTP/1.0") == 0) {
+        return 10;
+    } else if (strcmp(string, "HTTP/1.1") == 0) {
+        return 11;
+    } else if (strcmp(string, "HTTP/2.0") == 0) {
+        return 20;
+    } else {
+        printf("Invalid version number\n");
+        return 0;
+    }
+}
 
 void initialize_server(struct server *s) {
     /* Zero initialize everything */
@@ -46,7 +81,7 @@ void initialize_server(struct server *s) {
     s->servaddr.sin_port = htons(PORT);
 
     /* Bind socket */
-    bind(s->listenfd, (struct sockaddr *)&(s->servaddr), sizeof(s->servaddr));
+    bind(s->listenfd, (struct sockaddr *)&s->servaddr, sizeof(s->servaddr));
     listen(s->listenfd, BACKLOG);
     
     /* HTTP Response OK */
@@ -55,13 +90,45 @@ void initialize_server(struct server *s) {
     printf("Server starting...\n");
 }
 
-int parse_request(char *buffer) {
-    return 0;
+struct http_request *parse_request(char const *const buffer) {
+    struct http_request *request = (struct http_request *)malloc(sizeof(struct http_request));
+    if (request == NULL) {
+        fprintf(stderr, "Error: parse_request: bad malloc\n");
+        return NULL;
+    }
+    memset(request, 0, sizeof(struct http_request));
+
+    char word[1024];
+    int index = 0;
+    int length = strlen(buffer);
+
+    for (int i = 0; i < length; i++) {
+        if (isspace(buffer[i])) {
+            word[index] = '\0';
+            if (request->mthd == UNDEFINED) {
+                request->mthd = get_method(word);
+                strcpy(request->method_name, word);
+            } else if (request->uri[0] == '\0') {
+                strcpy(request->uri, word);
+            } else if (request->version == 0) {
+                request->version = get_version(word);
+            } else {
+                break;
+            }
+            index = 0;
+        } else {
+            word[index] = buffer[i];
+            index++;
+        }
+    }
+    return request;
 }
 
 void run() {
     struct server serv;
     initialize_server(&serv);
+
+    struct http_request *request = NULL;
 
     char addr[INET_ADDRSTRLEN];
     int bytes = 0;
@@ -82,8 +149,13 @@ void run() {
             goto cleanup;
         }
         serv.recvbuf[bytes] = '\0';
-        printf("%s\n", serv.recvbuf);
-        
+        printf("Request: %s\n", serv.recvbuf);
+
+        request = parse_request(serv.recvbuf);
+        printf("Method: %s %d\n", request->method_name, request->mthd);
+        printf("Uri: %s\n", request->uri);
+        printf("Version: %d\n", request->version);
+
         bytes = write(serv.connfd, serv.sendbuf, strlen(serv.sendbuf));
         if (bytes < 0) {
             perror("write");
